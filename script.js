d976795bf9e5130d31d50c0b011580ee62481fb6
Praveen K <118113479+praveen9655@users.noreply.github.com>
2023-07-17 21:20:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let inText = document.getElementById('inText');
 let inImg = document.getElementById('qrImage');
 let errMsg = document.getElementById('errMsg');
-
 function qrGen(){
     if(inText.value == ''){
         errMsg.style.display = 'block';
+
     }
+
     else{
         errMsg.style.display = 'none';
         inImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+ inText.value;
